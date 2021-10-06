639ea599a22136dfe6d95deaf9bd70e864afc5a6
snakers41 <aveysov@gmail.com>
2021-10-06 10:48:31 +0000

    Refactor imports

--- hubconf.py
+++ hubconf.py
@@ -2,13 +2,6 @@ dependencies = ['torch', 'omegaconf', 'torchaudio']
 import os
 import torch
 from omegaconf import OmegaConf
-from utils import (init_jit_model,
-                   read_audio,
-                   read_batch,
-                   split_into_batches,
-                   prepare_model_input)
-from tts_utils import apply_tts
-from tts_utils import init_jit_model as init_jit_model_tts
 
 
 def silero_stt(language='en',
@@ -20,6 +13,11 @@ def silero_stt(language='en',
     Returns a model, decoder object and a set of utils
     Please see https://github.com/snakers4/silero-models for usage examples
     """
+    from utils import (init_jit_model,
+                       read_audio,
+                       read_batch,
+                       split_into_batches,
+                       prepare_model_input)
     torch.hub.download_url_to_file('https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml',
                                    'latest_silero_models.yml',
                                    progress=False)
@@ -45,6 +43,8 @@ def silero_tts(language='en',
     Returns a model and a set of utils
     Please see https://github.com/snakers4/silero-models for usage examples
     """
+    from tts_utils import apply_tts
+    from tts_utils import init_jit_model as init_jit_model_tts
     torch.hub.download_url_to_file('https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml',
                                    'latest_silero_models.yml',
                                    progress=False)
